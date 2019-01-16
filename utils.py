b01eda3c2fa30c8e0e80a20adaa93639a29c71c1
Curt the Guardian <guardian.curt@outlook.com>
2019-01-16 15:23:33 +0800

    Update: log file name and shorten the log length

--- utils.py
+++ utils.py
@@ -13,15 +13,11 @@ def log(*args, **kwargs):
     localtime = time.localtime(int(time.time()))
     formatted = time.strftime(time_format, localtime)
 
-    with open('log.gua.txt', 'a', encoding='utf-8') as f:
+    with open('log.txt', 'a', encoding='utf-8') as f:
         # 时间和log内容分开, 并且加上分隔符号
-        print(f'{formatted}')
+        print(f'[{formatted}]')
         print(*args, **kwargs)
-        print('=' * 50)
-        print()
 
         # 将log的内容写到文件里, 与之前的print分开是为了避免写文件太慢影响print
-        print(f'{formatted}', file=f)
+        print(f'[{formatted}]', file=f)
         print(*args, **kwargs, file=f)
-        print('=' * 50, file=f)
-        print('', file=f)
